bbabf0e192bb27f059048d9971cbbaa40869732c
Joshua Jarvis <jmj3886@rit.edu>
2017-10-10 17:16:37 -0400

    Added code to turn on Heater with raw IR code

--- IRController/IRController/IRController.ino
+++ IRController/IRController/IRController.ino
@@ -11,7 +11,43 @@
 // All of the above automatically creates a universal sending
 // class called "IRsend" containing only the protocols you want.
 // Now declare an instance of that sender.
+#include <IRLib_HashRaw.h>    //Only use raw sender
 
+#define POWER_DATA_LEN 100
+uint16_t powerData[POWER_DATA_LEN]={
+  1270, 450, 1258, 438, 410, 1258, 1286, 434, 
+  1258, 438, 410, 1258, 434, 1262, 430, 1262, 
+  434, 1262, 434, 1262, 430, 1262, 1282, 7190, 
+  1282, 438, 1258, 434, 398, 1274, 1286, 434, 
+  1258, 410, 422, 1274, 434, 1262, 430, 1262, 
+  434, 1262, 430, 1266, 430, 1262, 1282, 7186, 
+  1274, 450, 1258, 414, 430, 1262, 1282, 438, 
+  1258, 434, 410, 1262, 430, 1266, 430, 1262, 
+  430, 1266, 430, 1266, 430, 1262, 1282, 7186, 
+  1282, 442, 1258, 434, 410, 1262, 1282, 414, 
+  1282, 434, 410, 1262, 430, 1266, 430, 1262, 
+  430, 1266, 430, 1262, 434, 1262, 1282, 7186, 
+  1274, 426, 1270, 1000};
+
+#define UP_DATA_LEN 24
+uint16_t upData[UP_DATA_LEN]={
+  1198, 502, 1234, 458, 390, 1306, 1194, 502, 
+  1238, 454, 390, 1302, 378, 1318, 1238, 458, 
+  390, 1302, 378, 1318, 394, 1298, 378, 1000};
+
+#define DOWN_DATA_LEN 24
+uint16_t downData[DOWN_DATA_LEN]={
+  1202, 498, 1286, 410, 438, 1254, 1282, 414, 
+  1254, 442, 422, 1270, 1206, 490, 390, 1302, 
+  394, 1302, 358, 1338, 358, 1334, 362, 1000};
+
+#define ROTATE_DATA_LEN 24
+uint16_t rotateData[ROTATE_DATA_LEN]={
+  1198, 498, 1238, 458, 390, 1302, 1198, 498, 
+  1238, 458, 390, 1302, 374, 1318, 358, 1338, 
+  1198, 498, 390, 1302, 378, 1318, 374, 1000};
+
+IRsendRaw myRawSender;
 IRsend mySender;
 String selection = "0"; 
 
@@ -48,16 +84,16 @@ void loop() {
     mySender.send(NEC,0x5FA8A75,0);
   }else if(selection=="H1\n"){
     Serial.println("Power");
-    mySender.send(UNKNOWN,0xF6D1C6CE,48);
+    myRawSender.send(powerData,POWER_DATA_LEN,36);
   }else if(selection=="H2\n"){
     Serial.println("Temp Up");
-    mySender.send(UNKNOWN,0x39D41DC6,10);
+    myRawSender.send(upData,UP_DATA_LEN,36);
   }else if(selection=="H3\n"){
     Serial.println("Temp Down");
-    mySender.send(UNKNOWN,0xF6D1C6CE,48);
+    myRawSender.send(downData,DOWN_DATA_LEN,36);
   }else if(selection=="H4\n"){
     Serial.println("Rotate");
-    mySender.send(UNKNOWN,0xF6D1C6CE,48);
+    myRawSender.send(rotateData,ROTATE_DATA_LEN,36);
   }
   
   selection = "0";
