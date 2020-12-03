0cdcb18c65b5cd4d13f89879f4e6536429fabce1
Josh Jarvis <jmj3886@rit.edu>
2020-12-03 10:47:49 -0500

    Update GideonSend.js

--- GideonCommand/GideonSend.js
+++ GideonCommand/GideonSend.js
@@ -1,30 +1,89 @@
-// serial port initialization:
-var SerialPort = require('serialport'); // include the serialport library
-var portName = process.argv[2]; // get the port name from the command line
-var myPort = new SerialPort(portName, 9600);// open the port
- 
-// these are the definitions for the serial events:
-myPort.on('open', openPort); // called when the serial port opens
- 
-function openPort() {
-  var brightness = 0; // the brightness to send for the LED
-  console.log('port open');
-  console.log('baud rate: ' + myPort.options.baudRate);
- 
-  // since you only send data when the port is open, this function
-  // is local to the openPort() function:
-  function sendData() {
-    // convert the value to an ASCII string before sending it:
-    myPort.write(brightness.toString());
-    console.log('Sending ' + brightness + ' out the serial port');
-    // increment brightness by 10 points. Rollover if < 255:
-    if (brightness < 255) {
-      brightness+= 10;
-    } else {
-      brightness = 0;
+// SERIAL PORT INITIALIZATION:
+var serialPort = require('serialport'); // SERIAL PORT LIBRARY
+var portName = process.argv[2]; // SPECIFY PORTNAME FROM CMD ARGUMENT
+var commPort = new SerialPort(portName, 9600, openPort); // CREATES NEW SERIAL PORT
+
+commPort.on('data', dataReceived) // CALLS dataReceived WHEN DATA IS AVAILABLE
+
+function openPort()
+{
+    console.log('Serial Port \''+portName+'\' Connected');
+    console.log('\tBaudRate: ' + commPort.options.baudRate);
+}
+
+function dataReceived(data)
+{
+    console.log('Serial Message Received: ', data);
+}
+
+function sendCommMessage(command, protocol, messageLength, message, optional_message)
+    {
+        if(commPort.isOpen())
+        {
+            commPort.write(command);
+            commPort.write(protocol);
+            if(command == "SEND")
+            {
+                commPort.write(messageLength);
+                for(let messagePart of message)
+                {
+                    commPort.write(messagePart);
+                }
+                commPort.write(optional_message);
+            }
+        }
+        else
+        {
+            console.log('Serial Port \''+portName+'\' Not Yet Connected');
+        }
   }
+
+
+//GIDEON COMMAND INITIALIZATION
+var christmasModeToggle = False;
+
+function christmasModeOn()
+{
+    sendCommMessage("SEND", "RF", "1", ["13340"], "5"); // MAIN TREE ON
+    christmasModeToggle = True;
 }
+
+function christmasModeOff()
+{
+    sendCommMessage("SEND", "RF", "1", ["13340"], "5"); // MAIN TREE OFF
+    christmasModeToggle = False;
+}
+
+
+//HTTP SERVER INITIALIZATION:
+var http = require('http'); // HTTP LIBRARY
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/christmasmode":
+            if(christmasModeToggle == True)
+            {
+                christmasModeOff();
+            }
+            else
+            {
+                christmasModeOn();
+            }
+            break;
+    }
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
+
+//TIMER INITIALIZATION
+var schedule = require('node-schedule'); // SCHEDULER LIBRARY
  
-// set an interval to update the brightness 2 times per second:
-setInterval(sendData, 500);
-}
+var christmasTimerOn = schedule.scheduleJob('30 15 * * *', christmasModeOn);
+var christmasTimerOff = schedule.scheduleJob('0 23 * * *', christmasModeOff);
